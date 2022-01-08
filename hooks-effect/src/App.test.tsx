3e7509447382b121214bdab1dbccd2803f477517
paolo.carraro <paolo.carraro@interlogica.it>
2022-01-08 13:56:49 +0100

    Removed act in unit test due suggest of eslint

--- hooks-effect/src/App.test.tsx
+++ hooks-effect/src/App.test.tsx
@@ -1,22 +1,17 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
-import { act } from "react-dom/test-utils";
 
 describe("renders learn react link", () => {
   it("should init app", () => {
-    act(() => {
-      render(<App />);
-    });
+    render(<App />);
 
     const mtComp = screen.getByTestId("mouse-tracker");
     expect(mtComp).toBeInTheDocument();
   });
 
   it("should remove comp", () => {
-    act(() => {
-      render(<App />);
-    });
+    render(<App />);
 
     const toggleBtn = screen.getByLabelText("toggle-button");
     let mtComp = screen.getByTestId("mouse-tracker");
--- hooks-effect/src/features/mouse-tracker/MouseTracker.test.tsx
+++ hooks-effect/src/features/mouse-tracker/MouseTracker.test.tsx
@@ -1,21 +1,16 @@
 import { fireEvent, render, screen } from "@testing-library/react";
-import { act } from "react-dom/test-utils";
 import MouseTracker from "./MouseTracker";
 
 describe("Mouse track", () => {
   it("should init component", () => {
-    act(() => {
-      render(<MouseTracker />);
-    });
+    render(<MouseTracker />);
 
     const cf = screen.getByTestId("coord-feedback");
     expect(cf.textContent?.replace(/\s/g, " ")).toBe("X: 0 Y: 0");
   });
 
   it("should handle mouse move", () => {
-    act(() => {
-      render(<MouseTracker />);
-    });
+    render(<MouseTracker />);
 
     const cf = screen.getByTestId("coord-feedback");
 
